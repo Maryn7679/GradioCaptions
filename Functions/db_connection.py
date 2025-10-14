cfe36900ad392c03ff826b944ce57d777bf4f45d
Maryna Kosse <mkosse@kse.org.ua>
2025-10-14 20:14:05 +0300

    Path update

--- Functions/db_connection.py
+++ Functions/db_connection.py
@@ -1,15 +1,10 @@
 import firebase_admin
 from firebase_admin import db
 
-KEY_PATH = 'Pages/Resources/key.json'
+KEY_PATH = 'Resources/key.json'
 
 cred_obj = firebase_admin.credentials.Certificate(KEY_PATH)
 default_app = firebase_admin.initialize_app(cred_obj, {
     'databaseURL': "https://video-link-db-default-rtdb.europe-west1.firebasedatabase.app/"
     })
 videos_ref = db.reference("/Videos")
-users_ref = db.reference("/Users")
-
-
-def get_user_ref(user):
-    return db.reference("/Users/" + user)
