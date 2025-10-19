b2efa0d0d94eea409dc54cdbfef17b8e9277abbe
Maryna Kosse <mkosse@kse.org.ua>
2025-10-19 18:13:30 +0300

    Key authentication 2

--- Functions/db_connection.py
+++ Functions/db_connection.py
@@ -1,9 +1,14 @@
+import json
+
 import firebase_admin
 from firebase_admin import db
+import os
 
 # KEY_PATH = 'Resources/key.json'
+key = os.getenv('FIREBASE_KEY')
+key_json = json.loads(key)
 
-cred_obj = firebase_admin.credentials.Certificate(FIREBASE_KEY)
+cred_obj = firebase_admin.credentials.Certificate(key)
 default_app = firebase_admin.initialize_app(cred_obj, {
     'databaseURL': "https://video-link-db-default-rtdb.europe-west1.firebasedatabase.app/"
     })
