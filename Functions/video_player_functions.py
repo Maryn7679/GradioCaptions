b60d1978e8b6874a6bd01bc7e7e347fa14560c2d
Maryna Kosse <mkosse@kse.org.ua>
2025-10-26 21:49:27 +0200

    Error test 3

--- Functions/video_player_functions.py
+++ Functions/video_player_functions.py
@@ -1,6 +1,20 @@
 import re
-from Functions.db_connection import videos_ref
+# from Functions.db_connection import videos_ref
+import firebase
 
+firebaseConfig = {
+                  "apiKey": "AIzaSyBVTnll2fdV3qkQ4EFG3hsRMO6P4phW7Kc",
+                  "authDomain": "video-link-db.firebaseapp.com",
+                  "databaseURL": "https://video-link-db-default-rtdb.europe-west1.firebasedatabase.app",
+                  "projectId": "video-link-db",
+                  "storageBucket": "video-link-db.firebasestorage.app",
+                  "messagingSenderId": "777912710342",
+                  "appId": "1:777912710342:web:9ac9387604fc35262953c7"
+}
+
+default_app = firebase.initialize_app(firebaseConfig)
+db = default_app.database()
+videos_ref = db.child("Videos")
 
 def youtube_link_to_id(link):
     print(f"youtube_link_to_id video link: {link}")
