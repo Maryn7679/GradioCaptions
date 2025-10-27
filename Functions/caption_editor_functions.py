06610990b5051d12632df35e881f70bddca38620
Maryna Kosse <mkosse@kse.org.ua>
2025-10-27 20:53:16 +0200

    DB storage debug 3

--- Functions/caption_editor_functions.py
+++ Functions/caption_editor_functions.py
@@ -4,13 +4,22 @@ from Functions.db_connection import default_app
 
 def get_captions_by_video_id(video_id):
     response = default_app.database().child("Captions").child(video_id).get().val()
-    captions = pd.DataFrame(response)
+    if response is None:
+        captions = pd.DataFrame(columns=["clean_text", "end_time", "signer", "start_time", "text", "user_id"])
+    else:
+        captions = pd.DataFrame(response)
     captions_edit = captions[['start_time', 'text', 'end_time']]
     captions_edit.columns = ["Start", "Text", "End"]
     return captions_edit, captions
 
 
 def save_dataframe(df, df_full, video_id, user):
+    if len(df.index) > len(df_full.index):
+        new_rows = len(df_full.index) - len(df.index)
+        empty_data = {col: [None for _ in range(new_rows)] for col in df_full.columns}
+        empty_df = pd.DataFrame(empty_data)
+
+        df_full = pd.concat([df_full, empty_df], ignore_index=True)
     try:
         df_full["user_id"].loc[
             df_full["start_time"] != df["Start"] or
@@ -21,7 +30,7 @@ def save_dataframe(df, df_full, video_id, user):
         df_full["text"] = df["Text"]
         df_full["end_time"] = df["End"].apply(lambda x: float(x))
 
-        df_json = df.to_json(orient="index")
+        df_json = df_full.to_json(orient="index")
         default_app.database().child("Captions").child(video_id).set(df_json)
 
         return "Save successful!"
