291a36d3da9dcb35581ae9789ccf33dee04363c7
Maryna Kosse <mkosse@kse.org.ua>
2025-10-27 21:46:11 +0200

    Config

--- Functions/db_connection.py
+++ Functions/db_connection.py
@@ -1,13 +1,8 @@
 import firebase
+import json
+import os
 
-firebaseConfig = {
-                  "apiKey": "AIzaSyBVTnll2fdV3qkQ4EFG3hsRMO6P4phW7Kc",
-                  "authDomain": "video-link-db.firebaseapp.com",
-                  "databaseURL": "https://video-link-db-default-rtdb.europe-west1.firebasedatabase.app",
-                  "projectId": "video-link-db",
-                  "storageBucket": "video-link-db.firebasestorage.app",
-                  "messagingSenderId": "777912710342",
-                  "appId": "1:777912710342:web:9ac9387604fc35262953c7"
-}
+firebaseConfig = os.getenv("FIREBASE_CONFIG")
+config_json = json.loads(firebaseConfig)
 
-default_app = firebase.initialize_app(firebaseConfig)
+default_app = firebase.initialize_app(config_json)
